aa6b74239ce61868b29606c097dc2764755e1496
Anon-Ecology <75208283+Anon-Ecology@users.noreply.github.com>
2025-01-15 14:38:57 +0000

    Update Rast_to_CSV file

--- Rast_to_CSV.R
+++ Rast_to_CSV.R
@@ -1,15 +1,15 @@
 #load libraries
 library(terra)
-library(tidyterra)
 library(data.table)
 
 #set working directory
 setwd("/Users/user/KEW/Cluster_Code")
-#load tif raster to be converted
+
 #requires laptop with decent RAM, and potentially increasing vector size limit for R
 #on windows use this https://www.rdocumentation.org/packages/utils/versions/3.4.1/topics/memory.size
 
-rast<-rast("centre_agro_stack.tif")
-df<-as.data.frame(rast, cell=TRUE, xy=FALSE)#convert rast to dataframe
+#load tif raster to be converted
+rast<-rast("climate_input_unrounded.tif")
+df<-as.data.frame(rast, cell=TRUE, xy=TRUE)#convert rast to dataframe
 dt<-setDT(df)#convert dataframe to datatable (better for large exports to csv)
-fwrite(dt, "Converted_Rast.csv")#runs in parallel, much faster than write.csv
+fwrite(dt, "climate_input_unrounded.csv")#runs in parallel, much faster than write.csv
