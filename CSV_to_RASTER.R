64497eda3214095e057dbcdae23e91cb23d9d5b9
Anna-Lee-Jones <anna.jones@wolfson.ox.ac.uk>
2025-07-10 21:33:14 +0100

    remove old files

--- CSV_to_RASTER.R
+++
@@ -1,9 +0,0 @@
-#load libraries
-library(terra)
-library(tidyterra)
-library(data.table)
-
-df<- fread("~/OUPUT_TR_1km_T1T2_slope_dist.csv")
-dist_rast<-as_spatraster(df, xycols = 2:3, crs="EPSG:4326", digits=1)
-writeRaster(dist_rast, "~/OUTPUT_TR_1km_T1T2_slope_dist.tif")
-
